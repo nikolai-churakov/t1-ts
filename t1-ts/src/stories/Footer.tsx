36f98847abe65c83989c37e138137fa342ff9511
Nikolai <cni.zip@gmail.com>
2024-02-13 23:35:30 +0300

    add storyBook

---
+++ t1-ts/src/stories/Footer.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {Container, MenuContainer, LogoName, MenuButtons, Button} from "./Footer.styled";
+
+export const Footer = () => {
+  return (
+    <Container>
+      <MenuContainer>
+        <LogoName>Goods4you</LogoName>
+
+        <MenuButtons>
+          <Button>Catalog</Button>
+
+          <Button>About us</Button>
+
+          <Button>Product selection</Button>
+
+          <Button>Out team</Button>
+
+          <Button>Shipping and payment</Button>
+
+          <Button>Contacts</Button>
+        </MenuButtons>
+      </MenuContainer>
+    </Container>
+  );
+}
