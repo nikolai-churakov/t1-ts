088fab0f56814a762e7ed472ae81bc471065abad
Nikolai <cni.zip@gmail.com>
2024-03-01 21:40:49 +0300

    ad

--- t1-ts/src/App.tsx
+++ t1-ts/src/App.tsx
@@ -4,11 +4,32 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Catalog } from "./pages/Catalog";
 import { ProductsList } from "./pages/ProductsList";
 
+// interface BrowserRouterProps {
+//   basename?: string;
+//   children?: React.ReactNode;
+//   future?: FutureConfig;
+//   window?: Window;
+// }
 
 
 function App() {
     
   return <Pages />;
+
+//   return (
+//     <BrowserRouter basename="/Pages">
+//       <Routes>
+//         <Route path="/t1-ts/"/> {}
+        
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// function Pages() {
+    
+//   return <Pages />;
+
 }
 
 export default App;
